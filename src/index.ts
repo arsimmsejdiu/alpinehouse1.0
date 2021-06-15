0b5d2ef5713083aecade75aa574a3c9d0d494f52
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-15 22:53:27 +0200

    it works

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,12 @@
 import express from "express";
-import bodyParser from "body-parser";
-const app = express();
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
+const app = express();
 const port = 1992;
 
-app.use(bodyParser.json())
-//Listings Routes
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-})
-//Delete Listings
-app.post('/delete-listings', (req,res) => {
-  const id: String = req.body.id;
-  for( let i = 0 ; i < listings.length ; i++ ) {
-    if(listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send("Failed to delete listings ... ... ... ")
-})
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api"})
 
 app.listen(port);
 console.log(`[app]: http://localhost:${port}`);
