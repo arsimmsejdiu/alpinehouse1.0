afff3d72e4faa78afd5478131cebbab7810bb0da
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-09-13 18:20:38 +0200

    Finished diplaying the listings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Listings } from './sections';
-import './App.css';
+import React from "react";
+import { Listings } from "./sections";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <Listings title="AlpineHouse Listings" />
     </div>
   );
--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -34,7 +34,7 @@ interface Props {
 }
 
 export const Listings = ({ title }: Props) => {
-  const { data, refetch } = useQuery<ListingsData>(LISTINGS)
+  const { data, refetch } = useQuery<ListingsData>(LISTINGS);
 
   const deleteListing = async (id: string) => {
     await server.fetch<DeleteListingData, DeleteListingVariables>({
@@ -49,23 +49,55 @@ export const Listings = ({ title }: Props) => {
   const listings = data ? data.listings : null;
 
   const listingList = listings ? (
-    <ul>
+    <div>
       {listings.map((listing) => {
         return (
-          <li key={listing.id}>
-            {listing.title} {" "}
-            <button onClick={() => deleteListing(listing.id.toString())}>
-              Delete
-            </button>
-          </li>
+          <div
+            className="flex py-8 px-2 pr-4 border-b shadow-sm hover:shadow-lg cursor-pointer hover:opacity-80 rounded-lg
+            active:bg-gray-100 transition duration-100 ease-out first:border-t"
+          >
+            <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0 hover:scale-105 transition transform ease-out">
+              <img
+                src={listing.image}
+                alt={listing.title}
+                className="rounded-2xl"
+              />
+            </div>
+            <div className="flex flex-col flex-grow pl-5">
+              <div className="flex justify-between">
+                <p>{listing.address}</p>
+              </div>
+              <h4 className="text-xl">{listing.title}</h4>
+              <div className="border-b w-10 pt-2" />
+              <p className="text-sm pt-2 text-gray-500 flex-grow">
+                number of guests: {listing.numOfGuests} {" | Beds: "}{" "}
+                {listing.numOfBeds} {" | Baths: "} {listing.numOfBaths}
+              </p>
+
+              <div className="flex justify-between items-end pt-5">
+                <div>
+                  <p className="text-lg lg:text-2xl font-semibold pb-2">
+                    ${listing.price}
+                  </p>
+                  <button
+                    className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 
+                    active:bg-red-50 transition transform duration-100 ease-out"
+                    onClick={() => deleteListing(listing.id.toString())}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         );
       })}
-    </ul>
+    </div>
   ) : null;
 
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className="text-gray-900 p-5 text-4xl border-b shadow-sm">{title}</h1>
       {listingList}
     </div>
   );
